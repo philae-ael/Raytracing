6df1a2feaa0a1df7021f3716252b8a832577f093
philae-ael <1511446+philae-ael@users.noreply.github.com>
2024-01-08 23:53:57 +0100

    some small refactor

--- cli/src/cli.rs
+++ cli/src/cli.rs
@@ -4,37 +4,39 @@ use anyhow::Result;
 
 use crate::{
     output::{FileOutput, TevStreaming},
-    tile_renderer::{TileMsg, TileRenderer},
-    Args, AvailableOutput, AvailableScene, Dimensions,
+    tile_renderer::{TileMsg, TileRenderer, TileRendererCreateInfo},
+    Args, AvailableOutput,
 };
 
 pub trait OutputStreaming: Send {
-    fn send_msg(&mut self, cli: &Cli, msg: &TileMsg) -> Result<()>;
+    fn send_msg(&mut self, msg: &TileMsg) -> Result<()>;
 }
 
 pub struct Cli {
     pub outputs: Vec<Box<dyn OutputStreaming>>,
-    pub dimensions: Dimensions,
-    pub tile_size: u32,
-    pub sample_per_pixel: u32,
-    pub scene: AvailableScene,
+    pub tile_renderer: TileRenderer,
 }
 
 impl Cli {
     pub fn new(args: Args) -> Result<Self> {
         let outputs: HashSet<AvailableOutput> = HashSet::from_iter(args.output.into_iter());
+        let tile_size = 20;
 
         let mut this = Self {
             outputs: Vec::new(),
-            dimensions: args.dimensions.clone(),
-            tile_size: 20,
-            sample_per_pixel: args.sample_per_pixel,
-            scene: args.scene,
+            tile_renderer: TileRenderer::new(TileRendererCreateInfo {
+                dimension: args.dimensions.clone(),
+                spp: args.sample_per_pixel,
+                tile_size,
+                scene: args.scene.into(),
+                shuffle_tiles: false,
+            }),
         };
 
         if outputs.contains(&AvailableOutput::Tev) {
             this.outputs.push(Box::new(TevStreaming::new(
-                &this,
+                args.dimensions,
+                tile_size,
                 args.tev_path,
                 args.tev_hostname,
             )?));
@@ -44,22 +46,15 @@ impl Cli {
     }
 
     pub fn run(mut self) -> Result<()> {
-        let mut outputs = Vec::new();
         let file_output = FileOutput::new(&self);
 
-        let output_buffers = TileRenderer {
-            width: self.dimensions.width,
-            height: self.dimensions.height,
-            spp: self.sample_per_pixel,
-            tile_size: 20,
-            scene: self.scene.into(),
-        }
-        .run(move |msg| {
+        let output_buffers = self.tile_renderer.run(|msg| {
+            let mut outputs = Vec::new();
             // Move tev_cli out of self, work with it and move it back in self
             std::mem::swap(&mut self.outputs, &mut outputs);
 
             for mut output in outputs.drain(..) {
-                match output.send_msg(&self, &msg) {
+                match output.send_msg(&msg) {
                     Ok(_) => self.outputs.push(output),
                     Err(err) => {
                         log::error!("{err}");
